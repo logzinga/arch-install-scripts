e3e66490473e1f1e00d3178f62c8ef1c4c7bb15c
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-30 12:31:56 +1000

    Update setup.sh
    - EFI System Partition (should) work

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "UEFI Is Required, are you on UEFI? [y/n]"
+read UEFICHECK
+if [ $UEFICHECK = n ]
+	then
+		echo "You Must use UEFI to Continue."
+		clear
+		exit
+fi
+
+clear
+
 echo "Have you connected to Internet? [y/n]"
 read INTERNETCONNECT
 if [ $INTERNETCONNECT = n ]
@@ -9,6 +20,8 @@ if [ $INTERNETCONNECT = n ]
 		exit
 fi
 
+clear
+
 timedatectl set-ntp true
 
 
@@ -17,4 +30,8 @@ fdisk -l
 echo "What type of drive are you using? [ sda/vda/nvme0n1 ]"
 read DRIVETYPE
 
-fdisk /dev/$DRIVETYPE
+clear
+
+parted /dev/$DRIVETYPE mklabel gpt
+parted /dev/$DRIVETYPE mkpart "EFI system partition" fat32 1MiB 301MiB
+parted /dev/$DRIVETYPE set 1 esp on
