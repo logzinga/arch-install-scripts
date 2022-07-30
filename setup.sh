4d3b1a1c5ba63a5bc67b3117540b160fc9dec572
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-30 13:23:44 +1000

    Setup.sh Complete

--- setup.sh
+++ setup.sh
@@ -34,6 +34,10 @@ read DRIVETYPE
 
 clear
 
+echo "Partitioning..."
+
+sleep 1
+
 parted /dev/$DRIVETYPE mklabel gpt
 parted /dev/$DRIVETYPE mkpart "EFI" fat32 1MiB 301MiB
 parted /dev/$DRIVETYPE set 1 esp on
@@ -45,4 +49,65 @@ parted /dev/$DRIVETYPE mkpart "swap" linux-swap 301MiB $SWAPAMOUNT
 
 parted /dev/$DRIVETYPE mkpart "root" ext4 $SWAPAMOUNT 100%
 
-mkfs.ext4 /dev/$DRIVETYPE3
+clear
+fdisk -l
+echo "What drive type are you using? [ sda/vda/nvme0n1 ]"
+read DRIVE2
+if [ $DRIVE2 = vda ]
+	then
+		mkfs.ext4 /dev/vda3
+        mkswap /dev/vda2
+        mkfs.fat -F 32 /dev/vda1
+
+        mount /dev/vda3 /mnt 
+        mount --mkdir /dev/vda1 /mnt/boot
+        swapon /dev/vda2
+fi
+if [ $DRIVE2 = sda ]
+	then
+		mkfs.ext4 /dev/sda3
+        mkswap /dev/sda2
+        mkfs.fat -F 32 /dev/sda1
+
+        mount /dev/sda3 /mnt 
+        mount --mkdir /dev/sda1 /mnt/boot
+        swapon /dev/sda2
+fi
+if [ $DRIVE2 = nvme0n1 ]
+	then
+		mkfs.ext4 /dev/nvme0n1p3
+        mkswap /dev/nvme0n1p2
+        mkfs.fat -F 32 /dev/nvme0n1p1
+
+        mount /dev/nvme0n1p3 /mnt 
+        mount --mkdir /dev/nvme0n1p1 /mnt/boot
+        swapon /dev/nvme0n1p2
+fi
+
+clear
+echo "Installing Arch Packages..."
+sleep 1
+pacstrap /mnt base linux linux-firmware nano
+
+clear
+echo "Generating fstab..."
+sleep 1
+genfstab -U /mnt >> /mnt/etc/fstab
+
+clear
+echo "Installing Git..."
+sleep 1
+pacstrap /mnt git
+
+clear
+echo "Installing NetworkManager..."
+sleep 1
+pacstrap /mnt networkmanager
+
+
+clear
+echo "When booting into the new system, make sure to do systemctl enable NetworkManager and systemctl start NetworkManager (with caps)."
+sleep 3
+echo "You will need to clone the git repository again and run setup2.sh to continue with the setup."
+arch-chroot /mnt
+
