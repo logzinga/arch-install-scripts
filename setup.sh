5f9a5929d51f49757c180144161604782a407a99
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-30 12:19:42 +1000

    nope

--- setup.sh
+++ setup.sh
@@ -18,29 +18,3 @@ echo "What type of drive are you using? [ sda/vda/nvme0n1 ]"
 read DRIVETYPE
 
 fdisk /dev/$DRIVETYPE
-
-g
-n
-1
-
-+300M
-
-t
-1
-
-n 
-2
-
-+16G
-
-t 
-2
-19
-
-n 
-3
-
-
-
-w 
-
